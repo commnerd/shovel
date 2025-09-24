34f1a5c43ce7c71c0334c14ce34711b62e2494c6
Michael J. Miller <commnerd@gmail.com>
2025-09-24 13:53:15 -0500

    Allow registration on main website

--- routes/auth.php
+++ routes/auth.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     // Allow registration in development
-    if (! app()->environment('production')) {
-        Route::get('register', [RegisteredUserController::class, 'create'])
-            ->name('register');
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
-        Route::post('register', [RegisteredUserController::class, 'store'])
-            ->name('register.store');
-    }
+    Route::post('register', [RegisteredUserController::class, 'store'])
+        ->name('register.store');
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
